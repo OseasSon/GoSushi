dd41f8da73cb92704ac914a2a7a143c0381d07f8
Oséas Filho <72031069+OseasSon@users.noreply.github.com>
2023-01-19 11:57:31 +0000

    Update Identity Verification

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
-window.intercomSettings = {
-api_base: "https://api-iam.intercom.io",
-app_id: "d90noo0b",
-};
+<script>
+  window.intercomSettings = {
+    api_base: "https://api-iam.intercom.io",
+    app_id: "d90noo0b",
+    email: "mytest.intercom@gmail.com", // User ID
+    user_hash: "tV6OeSGzXNSzu8I5Rff3ZOlHqbqKWwyY10j8Ktl1" // HMAC using SHA-256
+  };
+</script>
 
+<script>
 // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/d90noo0b'
 (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/d90noo0b';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-
+</script>
 
 /**
  * Check if the password entered is valid
